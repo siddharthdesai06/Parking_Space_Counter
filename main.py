35f85e811f883c974860e72fc52f5e35d4e8e176
Siddharth S Desai <101576956+siddharthdesai06@users.noreply.github.com>
2024-01-13 17:50:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,84 @@
+# import pickle
+
+# import numpy as np
+# import pickle
+# from flask import Flask,render_template,request
+
+
+# import cv2
+# import cvzone
+
+
+from flask import Flask, render_template,request
+
+import cv2
+import pickle
+import cvzone
+import numpy as np
+#
+# app = flask.Flask(__name__)
+#
+#
+# @app.route("/")
+# def man():
+#     return flask.render_template("home.html")
+#
+#
+# @app.route('/start', methods=['POST'])
+# def home():
+    # Video feed 30 frames per sec
+cap = cv2.VideoCapture('carPark.mp4')
+
+width, height = 107, 48
+
+with open('CarParkPos', 'rb') as f:
+    posList = pickle.load(f)
+
+    def checkParkingSpace(imgPro):
+
+        spaceCounter = 0
+
+        for pos in posList:
+            x, y = pos
+
+            imgCrop = imgPro[y:y + height, x:x + width]
+            # cv2.imshow(str(x*y), imgCrop)
+            count = cv2.countNonZero(imgCrop)
+
+            if count < 900:
+                spaceCounter += 1
+                color = (0, 255, 0)
+                thickness = 5
+            else:
+                color = (0, 0, 255)
+                thickness = 2
+
+            cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), color, thickness)
+            cvzone.putTextRect(img, str(count), (x, y + height - 3), scale=1, thickness=2, offset=0, colorR=color)
+
+        cvzone.putTextRect(img, f'Free: {spaceCounter}/{len(posList)}', (20, 50), scale=3, thickness=5, offset=20,
+                           colorR=(0, 200, 0))
+        pickle.dump(imgPro, open("psc.pkl", 'wb'))
+
+    while True:
+        if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        success, img = cap.read()
+        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
+        imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25,
+                                             16)
+        imgMedian = cv2.medianBlur(imgThreshold, 5)
+
+        kernel = np.ones((3, 3), np.uint8)
+        imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
+
+        checkParkingSpace(imgDilate)
+
+        cv2.imshow('Image', img)
+        cv2.waitKey(10)
+
+
+#
+if __name__ == "__main__":
+    app.run(debug=True)
